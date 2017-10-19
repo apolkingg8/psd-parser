dc659fcd9b19eb2de82d90287d696a28188a6831
Edward Chu <crazyzyt@gmail.com>
2017-10-19 15:25:20 +0800

    update: read psd asynchronously and return promise

--- index.js
+++ index.js
@@ -4,21 +4,23 @@
  */
 
 var parseHeader = require('./lib/header'),
-    parseColorMode = require('./lib/colorMode'),
-    parseImageResources = require('./lib/imageResources'),
-    parseLayerMaskInfo = require('./lib/layerMaskInfo'),
-    parseImageData = require('./lib/imageData'),
-    init = require('./lib/init'),
-    handle = require('./lib/handler');
+parseColorMode = require('./lib/colorMode'),
+parseImageResources = require('./lib/imageResources'),
+parseLayerMaskInfo = require('./lib/layerMaskInfo'),
+parseImageData = require('./lib/imageData'),
+init = require('./lib/init'),
+handle = require('./lib/handler');
 
-exports.parse = function(path){
-    var PSD = init(path);
-    parseHeader(PSD);
-    parseColorMode(PSD);
-    parseImageResources(PSD);
-    parseLayerMaskInfo(PSD);
-    parseImageData(PSD);
-    return handle(PSD);
+exports.parse = function parse (path) {
+    return init(path)
+    .then(PSD => {
+        parseHeader(PSD);
+        parseColorMode(PSD);
+        parseImageResources(PSD);
+        parseLayerMaskInfo(PSD);
+        parseImageData(PSD);
+        return handle(PSD);
+    })
 };
 //TODO 树形分层
 //一些get方法，如链接图层
